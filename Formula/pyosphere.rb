732f95eb35c3639834be8fa43b89aeb4db4b93e4
Vedant Puri <vedantpuri13@gmail.com>
2018-07-18 17:56:40 +0530

    Update formula for "vedantpuri/pyosphere" to 1.1.1

--- Formula/pyosphere.rb
+++ Formula/pyosphere.rb
@@ -1,9 +1,9 @@
 class Pyosphere < Formula
   desc "A script to enable easy and error-free imports in python."
   homepage "https://github.com/vedantpuri/pyosphere"
-  url "https://github.com/vedantpuri/pyosphere/archive/1.1.0.tar.gz"
-  version "1.1.0"
-  sha256 "0fba26013c481a3141bbe4e649ef413d7def2ea33dc1490823ff9212cb56d0b2"
+  url "https://github.com/vedantpuri/pyosphere/archive/1.1.1.tar.gz"
+  version "1.1.1"
+  sha256 "478f675b7f643a32dc3c1e567d4014c32bce2d249a7fbad2db1df902a56a2abe"
 
   def install
     bin.install "pyosphere.sh" => "pyosphere"
